c846265fe6d19e1859149df5244610dd9d8bdd3c
NecroDead <12.12.12.8.8.8a@gmail.com>
2023-03-13 16:36:26 +0300

    second

---
+++ source/repos/YP_l1/YP_l1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace YP_l1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guns guns = new Guns(Console.ReadLine(), Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            guns.Print();
+            Console.ReadKey();
+        }
+        class Guns
+        {
+            string name;
+            double caliber;
+            double range;
+            public void Print()
+            {
+                Console.WriteLine($"название: {name}\nкалибр: {caliber.ToString()}\nдальность: {range.ToString()}");
+            }
+            public Guns(string n, double c, double r)
+            {
+                name = n;
+                caliber = c;
+                range = r;
+            }
+        }
+    }
+}
